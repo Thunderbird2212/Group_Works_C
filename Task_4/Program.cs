f8e33b55921c61ce02873226f38c36f25ad21146
Kirill Romanov <freibird22@gmail.com>
2022-08-17 19:49:46 +0300

    Square number check out.

---
+++ Task_4/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+5, 25  ->  да
+-4, 16  ->  да
+25, 5  ->  да
+8,9  ->  нет*/
+
+Console.Write("Enter First Number: ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter Second Number: ");
+int second = Convert.ToInt32(Console.ReadLine());
+
+
+if (IsSqr(first, second))
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
+
+bool IsSqr(int num1, int num2)
+{
+    if (num1 * num1 == num2 || num2 * num2 == num1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
