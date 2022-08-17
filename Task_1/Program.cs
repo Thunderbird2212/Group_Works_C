0d17b52939e3bd0bd4dabdc88fdec23cc401f568
Kirill Romanov <freibird22@gmail.com>
2022-08-17 17:53:40 +0300

    Delete Second Digit in ThreeDigits.

---
+++ Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Программа принимает на вход трёхзначное число и выводит первую и последнюю цифру этого числа.*/
+
+int a = new Random().Next(100, 1000);
+Console.WriteLine(a);
+int result = deleteSecondNumber(a);
+Console.WriteLine(result);
+
+int deleteSecondNumber(int number)
+{
+    int digitOne = number / 100;
+    int digitThird = number % 10;
+    int resultTwoDigits = digitOne * 10 + digitThird;
+    return resultTwoDigits;
+
+}
